d3dd6f1d3a0bb3e71f5ce5b74df6b377e64c3e95
JohnGUnderwood <4124456+JohnGUnderwood@users.noreply.github.com>
2024-07-23 13:28:22 +0100

    Add display of index definitions to the UI

--- pages/api/indexes.js
+++ pages/api/indexes.js
@@ -6,15 +6,13 @@ const router = createRouter();
 router.use(database);
 
 router.get(async (req, res) => {
-    if(!req.query.type || (req.query.type != 'vector' && req.query.type != 'search')){
-        console.log(`Request missing 'type' parameter must be set to 'vector' or 'search'`)
-        res.status(400).send(`Request missing 'type' parameter must be set to 'vector' or 'search'`);
+    
+    if(req.query.type == 'vector'){
+        res.status(200).json(req.vectorIndex);
+    }else if(req.query.type == 'search'){
+        res.status(200).json(req.searchIndex);
     }else{
-        if(req.query.type == 'vector'){
-            res.status(200).json(req.vectorIndex);
-        }else if(req.query.type == 'search'){
-            res.status(200).json(req.searchIndex);
-        }
+        res.status(200).json({searchIndex:req.searchIndex,vectorIndex:req.vectorIndex});
     }
 });
 
--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,10 @@ import AppBanner from '../components/banner';
 import { ToastProvider, useToast } from '@leafygreen-ui/toast';
 import { Spinner } from "@leafygreen-ui/loading-indicator";
 import schema from '../config.mjs';
+import Modal from '@leafygreen-ui/modal';
+import { Subtitle } from '@leafygreen-ui/typography';
+import Code from '@leafygreen-ui/code';
+import ExpandableCard from '@leafygreen-ui/expandable-card';
 
 const Home = () => {
   const { pushToast } = useToast();
@@ -19,6 +23,8 @@ const Home = () => {
   const [queryVector, setQueryVector] = useState(null);
   const [selectedTab, setSelectedTab] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [indexes, setIndexes] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleSearch = () => {
     console.log("Search Clicked!")
@@ -37,16 +43,30 @@ const Home = () => {
     }
   }
 
-  const handleQueryChange = (event) => {setQuery(event.target.value);
-  };
+  const handleShowIndexes = () => {
+    console.log("Show Indexes Clicked!")
+    getIndexes()
+    .then(resp => {
+      console.log("Indexes Fetched!")
+      setIndexes(resp);
+      setOpen(true);
+    })
+    .catch(error => {
+      console.log(error);
+      pushToast({timeout:10000,variant:"warning",title:"API Failure",description:`Failed to fetch indexes. ${error}`});
+    });
+  }
+
+  const handleQueryChange = (event) => {setQuery(event.target.value);};
 
   return (
     <>
     <Header/>
     <AppBanner heading="Atlas Hybrid Search Tester"></AppBanner>
-    <div style={{display:"grid",gridTemplateColumns:"90% 120px",gap:"10px",alignItems:"start"}}>
+    <div style={{display:"grid",gridTemplateColumns:"85% 120px 120px",gap:"10px",alignItems:"start"}}>
       <div><SearchInput value={query} onChange={handleQueryChange} aria-label="some label" style={{marginBottom:"20px"}}></SearchInput></div>
       <div style={{maxWidth:"120px"}}><Button onClick={()=>handleSearch()} variant="primary">Vector Search</Button></div>
+      <div style={{maxWidth:"120px"}}><Button onClick={()=>handleShowIndexes()}>Show Indexes</Button></div>
     </div>
     {loading?<Spinner description="Loading..."/>:<></>}
     <Tabs style={{marginTop:"15px"}} setSelected={setSelectedTab} selected={selectedTab}>
@@ -63,6 +83,25 @@ const Home = () => {
         <RRF query={query} queryVector={queryVector} schema={schema}/>
       </Tab>
     </Tabs>
+    <Modal open={open} setOpen={setOpen}>
+      <ExpandableCard
+        title="Atlas Search"
+        darkMode={false}
+      >
+        <Code language={'javascript'}>
+          {indexes ? JSON.stringify(indexes.searchIndex,null,2) : "" }
+        </Code>
+      </ExpandableCard>
+      <br/>    
+      <ExpandableCard
+        title="Atlas Vector Search"
+        darkMode={false}
+      >
+        <Code language={'javascript'}>
+          {indexes ? JSON.stringify(indexes.vectorIndex,null,2) : "" }
+        </Code>
+      </ExpandableCard>   
+    </Modal>
     </>
   )
 }
@@ -71,6 +110,15 @@ async function embedQuery(query){
   try{
     const embeddingResp = await axios.get('api/embed?terms='+query);
     return embeddingResp.data;
+  }catch (e) {
+    throw e;
+  }
+}
+
+async function getIndexes(){
+  try{
+    const response = await axios.get('api/indexes');
+    return response.data;
   }catch (e) {
     throw e;
   }
